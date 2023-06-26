27283e1892a1dc8320ec9e2535bb14ae4483312d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-26 12:11:37 -0700

    designs and agora api and channel

---
+++ mychat/static/js/stream.js
@@ -0,0 +1,6 @@
+const App = 'ca36753ac8634a52a6a4883a21a4003e'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYHgdsdx6dS7PnU/6bjwaSzZ/d9t997Oj5PXY2z/5Ts5wOdmowJCcaGxmbmqcmGxhZmySaGqUaJZoYmFhnGhkmGhiYGCcyvRwZkpDICOD4OolDIxAyALEID4TmGQGkyxQMjcxM4+BAQAGAyLf'
+
+
+console.log('Stream connected')
