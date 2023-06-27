0ee87af88366f7c69304998adfaa72da57e4f7a8
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-27 00:24:40 -0700

    muting screen functionality

--- mychat/static/js/stream.js
+++ mychat/static/js/stream.js
@@ -75,7 +75,19 @@ let leaveAndRemoveLocalStream = async () => {
     window.open('/', '_self')
 }
 
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
 
 joinAndDisplayLocalStream();
 document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+
 
