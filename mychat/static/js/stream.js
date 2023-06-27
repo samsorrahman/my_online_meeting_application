f8cc102bd7417149e42495276b624da35160eeec
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-27 00:30:29 -0700

    mute functionality

--- mychat/static/js/stream.js
+++ mychat/static/js/stream.js
@@ -85,9 +85,20 @@ let toggleCamera = async (e) => {
     }
 }
 
+let toggleMic = async (e) => {
+    if(localTracks[0].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
 
 joinAndDisplayLocalStream();
 document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
 document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
 
 
